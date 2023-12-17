7ff373acb7688ec8f711135898a657d9e810acdc
TheStrechh <thestrechh@gmail.com>
2023-12-17 11:10:09 +0530

    extra: cleanup

--- product.mk
+++ product.mk
@@ -7,16 +7,13 @@
 # Inherit Android Go Makefile
 $(call inherit-product, vendor/extra/config/go.mk)
 
-# Inherit priv Makefile
-$(call inherit-product-if-exists, vendor/extra/priv/product.mk)
-
 # Boot animation
 TARGET_BOOTANIMATION_HALF_RES := true
 
 # Overlays
 PRODUCT_PACKAGES += \
     WebViewOverlay \
-    LineageUpdaterOverlay$(PRODUCT_VERSION_MAJOR) \
+    LineageUpdaterOverlay20 \
     RippleSystemUIOverlay
 
 # Rootdir
